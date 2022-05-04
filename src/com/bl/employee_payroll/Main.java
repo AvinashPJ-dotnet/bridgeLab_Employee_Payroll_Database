befbc68e562fdd16913d65d7d7743676cdcefa9e
AvinashPJ-dotnet <avinash.pujari42@gmail.com>
2022-05-04 16:14:26 +0530

    Added: updateEmployeePayrollTable method to update salary

---
+++ src/com/bl/employee_payroll/Main.java
@@ -0,0 +1,8 @@
+package com.bl.employee_payroll;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee_Payroll_Repository employee_payroll_repository = new Employee_Payroll_Repository();
+        employee_payroll_repository.start();
+    }
+}
--- src/com/bl/employee_payroll/test/EmployeePayrollTest.java
+++ src/com/bl/employee_payroll/test/EmployeePayrollTest.java
@@ -1,10 +1,14 @@
 package com.bl.employee_payroll.test;
 
+import com.bl.employee_payroll.Employee_Payroll_Repository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-
 public class EmployeePayrollTest {
-
+    @Test
+    public void givenSalary_MatchTheUpdatedSalary_ReturnTrue(){
+        Employee_Payroll_Repository employee_payroll_repository = new Employee_Payroll_Repository();
+        int count = employee_payroll_repository.updateEmployeePayroll("salary","3000000.00",5);
+        Assertions.assertEquals(1,count);
+    }
 }
